5c409d2e6500d7828cd615d1032dc287035e1ad0
Niachal Khanal <khanalnischal2075@gmail.com>
2024-01-12 08:41:16 +0545

    feat: project setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+import cors from 'cors';
+
+
+
+const server = express();
+let PORT = 3000;
+server.use(express.json());
+server.use(cors(
+    {
+        origin: 'https://laganiforum-backend.vercel.app',
+        credentials: true,
+        methods: ['GET', 'POST', 'OPTIONS']
+    }
+));
+server.options('*', cors());
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+});
+
+
+server.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+server.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+})
