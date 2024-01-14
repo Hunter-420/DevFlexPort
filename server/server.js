37c76cf342d19d742982301cd7a9beed4df0a55d
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-14 09:28:09 +0545

    chore(server): implementation of clean code concept     - server script changed, dev -> development server, start--> testing & production purposes     - use of promises in database connection code block     - .env-example added     - database connection .env variable name changed     - .env PORT variable introduced     - HTTP response code     - add comments     - license id changes     BREAKING CHANGES: reconfiguration of .env file is needed as per .env-example scheme

--- server/server.js
+++ server/server.js
@@ -1,24 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import 'dotenv/config'
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import cors from "cors";
 
-
-
+/* EXPRESS SERVER */
 const server = express();
-let PORT = 3000;
+
+/* BODY PARSER */
 server.use(express.json());
+
+/* CORS */
 server.use(cors());
-server.options('*', cors());
-mongoose.connect(process.env.DB_LOCATION, {
-    autoIndex: true
-});
+server.options("*", cors());
 
+/* DATABASE CONNECTION */
+mongoose.connect(process.env.MONGO_URI, {
+    autoIndex: true,
+  }).then(() => {
+      console.log("Database Connected");
+  })
+   .catch((err) => {
+      console.log(err);
+  })
 
-server.get('/', (req, res) => {
-    res.send('Hello World')
-})
+/* ROUTES */
+server.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+}); 
 
+/* SERVER LISTENING */
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT);
-})
+  console.log("Server listening on port " + PORT);
+});
