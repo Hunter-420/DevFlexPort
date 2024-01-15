abbbfc1c83125cbe3068b550f793efac08df9d9b
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-15 21:49:12 +0545

    chore(server): code readability - removed unecessary semicolumns - requests.rest gitignored - introduced MVC architechture

--- server/server.js
+++ server/server.js
@@ -1,35 +1,35 @@
-import express from "express";
-import mongoose from "mongoose";
-import "dotenv/config";
-import cors from "cors";
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import cors from 'cors'
 
 /* EXPRESS SERVER */
-const server = express();
+const server = express()
 
 /* BODY PARSER */
-server.use(express.json());
+server.use(express.json())
 
 /* CORS */
-server.use(cors());
-server.options("*", cors());
+server.use(cors())
+server.options('*', cors())
 
 /* DATABASE CONNECTION */
 mongoose.connect(process.env.MONGO_URI, {
     autoIndex: true,
   }).then(() => {
-      console.log("Database Connected");
+      console.log('Database Connected')
   })
    .catch((err) => {
-      console.log(err);
+      console.log(err)
   })
 
 /* ROUTES */
-server.get("/", (req, res) => {
-  res.status(200).send("Hello World");
-}); 
+server.get('/', (req, res) => {
+  res.status(200).send('Hello World')
+}) 
 
 /* SERVER LISTENING */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
-  console.log("Server listening on port " + PORT);
-});
+  console.log('Server listening on port ' + PORT)
+})
