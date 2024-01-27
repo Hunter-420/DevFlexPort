af91e1ded9a4010fed055fd5bd040907970853f3
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-27 10:08:07 +0545

    feat(server): user deletion feature unit testing is not necessary

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -206,11 +206,23 @@ const updateUser = async (req, res) => {
         
         await user.save()
         res.status(200).json({ message: 'User Profile updated successfully' })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const { userId } = req.params
+
+    try {
+        await User.findByIdAndDelete(userId)
+        res.status(200).json({ message: 'User deleted successfully' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 }
 
 export {
-    updateUser
+    updateUser,
+    deleteUser,
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/auth.js'
-import { updateUser } from '../controllers/user.js'
+import { updateUser, deleteUser } from '../controllers/user.js'
 
 const router = express.Router()
 
@@ -13,5 +13,6 @@ router
     .put('/:userId', verifyToken, updateUser)
 
     /* DELETE */
+    .delete('/:userId', verifyToken, deleteUser)
 
 export default router
