63ab11d79560e4ec19cb887c51249eb2cab2479c
Nischal Khanal <92971596+Hunter-420@users.noreply.github.com>
2024-01-31 19:11:24 +0545

    project card added (#24)

--- client/src/components/completeProjects.component.jsx
+++ client/src/components/completeProjects.component.jsx
@@ -1,9 +1,35 @@
-import '../css/completeProject.component.css'
-const CompleteProject = () => {
-    return (
-        <div className="completeProject">
-            <h1 className="text-white text-4xl">Complete Project Component</h1>
+import "../css/completeProject.component.css";
+const CompleteProject = ({
+  project_img,
+  project_tech,
+  project_title,
+  project_description,
+  live_link,
+  project_link,
+}) => {
+  return (
+    <div className="completeProject_container">
+      <div className="project_image">
+        <img src={project_img} alt="Project Image" />
+      </div>
+      <div className="project_tech">{project_tech}</div>
+      <div className="project_info">
+        <div className="project_title">{project_title}</div>
+        <div className="project_description">{project_description} </div>
+        <div className="project_link">
+          <div className="project_link_button">
+            <button className="project_link_button_text" href={live_link}>
+              Live
+            </button>
+          </div>
+          <div className="project_link_button">
+            <button className="project_link_button_text" href={project_link}>
+              GitHub
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default CompleteProject;
