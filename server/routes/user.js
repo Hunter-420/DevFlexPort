97fc6f87cef1f201603da1668473abec8c07ce27
ujjwalnp <dhakalujjwal99@gmail.com>
2024-02-10 19:40:19 +0545

    chore(api): extract userId through req.id instead req.params at updatePersonalInfo API

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ import express from 'express'
 import rateLimit from 'express-rate-limit'
 
 import { verifyToken } from '../middlewares/auth.js'
-import { getUserDetails, updatePersonalInfo, updateUser, deleteUser } from '../controllers/user.js'
+import { getUserDetails, updatePersonalInfo, deleteUser } from '../controllers/user.js'
 
 const router = express.Router()
 
@@ -13,8 +13,7 @@ router
     .get('/:userId', rateLimit({ windowMs: 60 * 1000, max: 5 }), getUserDetails)
 
     /* UPDATE */
-    .patch('/personalInfo/:userId', verifyToken, rateLimit({ windowMs: 60 * 1000, max: 25 }), updatePersonalInfo)
-    .put('/:userId', verifyToken, rateLimit({ windowMs: 60 * 1000, max: 25 }), updateUser)
+    .patch('/personalInfo/', verifyToken, rateLimit({ windowMs: 60 * 1000, max: 25 }), updatePersonalInfo)
 
     /* DELETE */
     .delete('/:userId', verifyToken,  rateLimit({ windowMs: 60 * 1000, max: 25 }), deleteUser)
