1550b1509e429a05333eec4d200c8a7a2a205c7a
ujjwalnp <dhakalujjwal99@gmail.com>
2024-02-14 17:42:05 +0545

    fix(rate-limit): denied unauthorized access

--- server/middlewares/rateLimit.js
+++ server/middlewares/rateLimit.js
@@ -1,15 +1,15 @@
-import rateLimit from "express-rate-limit"
+import rateLimit from 'express-rate-limit'
 
 const rateLimitOptions = {
-    general: { windowMs: 60 * 1000, max: 5 },
-    login: { windowMs: 60 * 1000, max: 3 },
-    api: { windowMs: 60 * 1000, max: 10 },
-    deny: { max: 0 },
-  }
-  
-  const rateLimitPerRequestType = (requestType) => {
-    const options = rateLimitOptions[requestType] || rateLimitOptions.deny
-    return rateLimit(options)
-  }
-  
-  export default rateLimitPerRequestType
+  general: { windowMs: 60 * 1000, max: 5 },
+  auth: { windowMs: 60 * 1000, max: 3 },
+  api: { windowMs: 60 * 1000, max: 10 },
+  deny: { windowMs: 60 * 60 * 24 * 1000, max: 1 },
+}
+
+const rateLimitPerRequestType = (requestType) => {
+  const options = rateLimitOptions[requestType] || rateLimitOptions.deny
+  return rateLimit(options)
+}
+
+export default rateLimitPerRequestType
