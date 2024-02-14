1b796f56f6126444b0b1e7214647383afc4d0088
ujjwalnp <dhakalujjwal99@gmail.com>
2024-02-14 12:16:18 +0545

    feat(rate-limit): implementaed rate limi per request type

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import rateLimit from 'express-rate-limit'
 import swaggerUi from 'swagger-ui-express'
 
+import rateLimitPerRequestType from '../middlewares/rateLimit.js'
+
 import authRouter from './auth.js'
 import userRouter from './user.js'
 import publicRouter from './public.js'
@@ -10,9 +12,9 @@ import swaggerDocument from '../docs/swagger-output.json' assert { type: 'json' 
 const router = express.Router()
 
 router
-    .use('/auth', rateLimit({ windowMs: 60 * 1000, max: 5 }), authRouter)
-    .use('/user', rateLimit({ windowMs: 60 * 1000, max: 30 }), userRouter)
-    .use('/public', rateLimit({ windowMs: 60 * 1000, max: 5 }), publicRouter)
-    .use('/docs', rateLimit({ windowMs: 60 * 1000, max: 5 }), swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use('/auth', rateLimitPerRequestType('auth'), authRouter)
+    .use('/user', rateLimitPerRequestType('api'), userRouter)
+    .use('/public', rateLimitPerRequestType('general'), publicRouter)
+    .use('/docs', rateLimitPerRequestType('api'), swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     
 export default router
